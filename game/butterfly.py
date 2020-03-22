06dd27286503cc380650f1c1f117177dab1fdff8
ddorn <diego.dorn@free.fr>
2020-03-22 23:02:12 +0100

    cleanup butterfly.py

--- game/butterfly.py
+++ game/butterfly.py
@@ -1,15 +1,14 @@
 from random import random, randrange
 from colorsys import rgb_to_hsv, hsv_to_rgb
 from os import path
-from time import sleep
+
 
 import pygame
 from glm import ivec2
-from .entity import Entity
-
-# pygame.init()
 
 from game.constants import SPRITES_DIR, ORANGE
+from .entity import Entity
+
 
 def clamp(x, mini=0, maxi=1):
     if mini > maxi:
@@ -39,80 +38,14 @@ def hsv2rgb(h, s, v):
     )
 
 
-# def get_butterfly(color=ORANGE, scale=5):
-#     """
-#     Return an animation of a butterfly of the given color
-
-#     :param color: RGB tuple
-#     :param scale:
-#     :return: a list of surfaces
-#     """
-
-#     image: pygame.SurfaceType = pygame.image.load(
-#         path.join(SPRITES_DIR, "butterfly-orange.png")
-#     )
-
-#     h, s, v = rgb2hsv(*color)
-#     brighter = hsv2rgb(h + 0.03, s + 0.1, v + 0.1)
-#     darker = hsv2rgb(h - 0.06, s - 0.1, v - 0.1)
-#     very_darker = hsv2rgb(h + 0.2, 0.5, 0.2)
-
-#     palette = [(1, 0, 1), (0, 0, 0), brighter, color, darker, very_darker]
-
-#     image.set_palette(palette)
-#     image.set_colorkey((1, 0, 1))  # index 0
-
-#     nb_frames = 4
-#     width = image.get_width() // nb_frames
-#     height = image.get_height()
-#     frames = [image.subsurface((width * i, 0, width, height)) for i in range(nb_frames)]
-
-#     frames = [
-#         pygame.transform.scale(fra, (width * scale, height * scale)) for fra in frames
-#     ]
-#     return frames
-
-
 def random_color():
     """Random RGB color of the rainbow"""
     return hsv2rgb(random(), 1, 1)
 
 
-# def show_butterflies():
-#     """
-#     Create window with a few butterflies flying
-#     """
-#     nb_butterfly = 40
-#     butterflies = [
-#         get_butterfly(random_color(), randrange(2, 6)) for _ in range(nb_butterfly)
-#     ]
-#     pos = [(randrange(10, 780), randrange(10, 480)) for _ in range(nb_butterfly)]
-
-#     display: pygame.SurfaceType = pygame.display.set_mode((800, 500))
-
-#     frame = 0
-#     while True:
-#         frame += 1
-
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 return
-
-#         # Clean screen
-#         display.fill((235, 235, 235))
-
-#         # Blit all butterflies
-#         for i in range(nb_butterfly):
-#             display.blit(butterflies[i][(frame + i) % 4], pos[i])
-
-#         pygame.display.update()
-#         sleep(0.1)
-
-
-# if __name__ == "__main__":
-#     show_butterflies()
-
 class Butterfly(Entity):
+    NB_FRAMES = 4
+
     def __init__(self, app, scene, color=ORANGE, scale=5, num=0):
         """
         :param app: our main App object
@@ -126,8 +59,15 @@ class Butterfly(Entity):
         
         self.num = num
 
-        fn = path.join(SPRITES_DIR,"butterfly-orange.png")
-        
+        self.frames = self.get_animation(color)
+        self.position = ivec2(randrange(10, self.app.size.x - 13 * scale), randrange(10, self.app.size.y - 13 * scale))
+
+        self.time = 0
+        self.frame = 0.0
+
+    def get_animation(self, color):
+        fn = path.join(SPRITES_DIR, "butterfly-orange.png")
+
         # load an image if its not already in the cache, otherwise grab it
         image: pygame.SurfaceType = self.app.load(fn,
             lambda: pygame.image.load(fn)
@@ -143,29 +83,24 @@ class Butterfly(Entity):
         image.set_palette(palette)
         image.set_colorkey((1, 0, 1))  # index 0
 
-        nb_frames = 4
-        width = image.get_width() // nb_frames
+        width = image.get_width() // self.NB_FRAMES
         height = image.get_height()
-        self.frames = [image.subsurface((width * i, 0, width, height)) for i in range(nb_frames)]
 
-        self.frames = [
-            pygame.transform.scale(fra, (width * scale, height * scale)) for fra in self.frames
+        frames = [image.subsurface((width * i, 0, width, height)) for i in range(self.NB_FRAMES)]
+        frames = [
+            pygame.transform.scale(fra, (width * self.scale, height * self.scale)) for fra in frames
         ]
-        
-        self.position = ivec2(randrange(10, 780), randrange(10, 480))
-                
-        self.time = 0
-        self.frame = 0.0
+
+        return frames
 
     def update(self, t):
-        
         self.time += t * 10
-        
+
     def render(self, camera):
         
         pos  =  self.position - camera.position
         
         pos *= self.z
         
-        self.app.screen.blit(self.frames[int(self.time + self.num) % 4], pos)
 
+        self.app.screen.blit(self.frames[int(self.time + self.num) % self.NB_FRAMES], pos)
