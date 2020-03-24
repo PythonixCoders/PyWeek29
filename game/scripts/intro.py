b8fb8685927b3091e3999243642dcab81184d022
Grady O'Connell <flipcoder@gmail.com>
2020-03-24 15:12:09 -0700

    reverted back intro script

--- game/scripts/intro.py
+++ game/scripts/intro.py
@@ -4,33 +4,33 @@ import pygame.mixer
 
 def script(app, scene):
 
-    yield lambda: scene.key(' ')
+    # yield lambda: scene.key(' ')
     
-    # terminal = app.state.terminal
-    # keys = scene.keys
+    terminal = app.state.terminal
+    keys = scene.keys
 
-    # scene.sky_color = "black"
-    # typ = pygame.mixer.Sound("data/sounds/type.wav")
+    scene.sky_color = "black"
+    typ = pygame.mixer.Sound("data/sounds/type.wav")
 
-    # msg = "Welcome to Butterfly Destroyers!"
-    # for i in range(len(msg)):
-    #     terminal.write(msg[i], (i, 0), "red")
-    #     typ.play()
-    #     yield scene.sleep(0.1)
+    msg = "Welcome to Butterfly Destroyers!"
+    for i in range(len(msg)):
+        terminal.write(msg[i], (i, 0), "red")
+        typ.play()
+        yield scene.sleep(0.1)
 
-    # while True:
+    while True:
         
-    #     terminal.write("Press any key to continue", (0, 2), "white")
+        terminal.write("Press any key to continue", (0, 2), "white")
         
-    #     yield scene.sleep(0.2)
-    #     if any(keys()):
-    #         break
+        yield scene.sleep(0.2)
+        if any(keys()):
+            break
         
-    #     terminal.clear(2)
+        terminal.clear(2)
         
-    #     yield scene.sleep(0.2)
-    #     if any(keys()):
-    #         break
+        yield scene.sleep(0.2)
+        if any(keys()):
+            break
 
 
     app.state = "game"
