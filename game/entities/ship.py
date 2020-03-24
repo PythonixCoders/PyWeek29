baa6f74e3d5a206cded00e5d2125a3a87bfa6708
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-03-24 12:32:23 +0400

    Made the player a bit slower

--- game/entities/ship.py
+++ game/entities/ship.py
@@ -6,7 +6,7 @@ import pygame
 
 
 class Ship(Player):
-    def __init__(self, app, scene, speed=vec3(1000, 1000, 200)):
+    def __init__(self, app, scene, speed=vec3(500, 500, 100)):
         super().__init__(app, scene, speed)
         path = os.path.join(SPRITES_DIR, "ship.png")
         self.img = self.app.load(path, lambda: pygame.image.load(path))
