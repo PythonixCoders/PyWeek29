6b30d6ab9c16c817614423eff55b3604a46fafc9
ddorn <diego.dorn@free.fr>
2020-03-25 16:22:34 +0100

    specify python >= 3.7

--- run.py
+++ run.py
@@ -2,8 +2,8 @@
 
 import sys
 
-if sys.version_info[0] <= 2:
-    sys.exit("This game requires python 3")
+if sys.version_info[0] != 3 or sys.version_info[1] < 7:
+    sys.exit("This game requires python 3.7")
 print("Python version:", sys.version)
 
 from game import main
